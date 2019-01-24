96a855484b7e643a1c04fa9e9d082bb6c4249da9
veronica <veronicadonoso1506@gmail.com>
2019-01-23 19:36:16 -0500

    03-seekDestroy exercise done

---
+++ 03-seekDestroy.js
@@ -0,0 +1,11 @@
+/* 3. *Seek and Destroy* You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments.
+Note
+You have to use the arguments object. */
+function destroyer (arr) {
+  let arg = Array.from(arguments).slice(1)
+  return arr.filter(function (val) {
+    return !arg.includes(val)
+  })
+}
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3)
