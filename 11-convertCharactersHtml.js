3f3f381a83c04c6b21d42f4665ec19c224b2ceaf
veronica <veronicadonoso1506@gmail.com>
2019-02-10 09:15:38 -0500

    11-convertCharactersHtml.js exercise done and README file update

---
+++ 11-convertCharactersHtml.js
@@ -0,0 +1,7 @@
+/* 11. *Convert HTML Entities* Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities. */
+function convertHTML (str) {
+  str = str.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&apos;')
+  return str
+}
+
+convertHTML('Dolce & Gabbana')
